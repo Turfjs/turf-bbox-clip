b0c35827ebc2a2aac85eb2a9601ce9310b5592a1
Tim Channell <tim.channell@gmail.com>
2015-09-24 11:16:28 -0400

    eslint, doc

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var lineclip = require('lineclip'),
-  fc = require('turf-featurecollection')
+var lineclip = require('lineclip');
 
-
+/**
+ * Takes a {@link Feature} and a bbox and clips the feature to the bbox using [lineclip](https://github.com/mapbox/lineclip).
+ *
+ * @module turf/bbox-clip
+ * @category transformation
+ * @param {(Feature<LineString>|Feature<MultiLineString>|Feature<Polygon>|Feature<MultiPolygon>)} feature feature to clip to the bbox
+ * @param {Array<number>} bbox an Array of bounding box coordinates in the form: ```[xLow, yLow, xHigh, yHigh]```
+ * @return {(Feature<Polygon>|Feature<MultiPolygon>|Feature<MultiLineString>)}
+ * @example
+ * var bbox = [0, 0, 10, 10];
+ * var poly = turf.polygon([[[2, 2], [8, 4], [12, 8], [3, 7], [2, 2]]]);
+ *
+ * var result = turf.bboxClip(poly, bbox);
+ *
+ * //=result
+ */
 module.exports = function(feature, bbox) {
   var geom = feature;
   if (feature.geometry) {
     geom = feature.geometry;
   }
 
-  if (geom.type == "LineString") {
-    var clipped = lineclip(geom.coordinates, bbox);
-
-    return buildFeature("MultiLineString", clipped, feature.properties);
-  } if (geom.type == "MultiLineString") {
+  if (geom.type === 'LineString') {
+    return buildFeature('MultiLineString', lineclip(geom.coordinates, bbox), feature.properties);
+  } if (geom.type === 'MultiLineString') {
     var lines = [];
     geom.coordinates.forEach(function(line) {
       lineclip(line, bbox).forEach(function(result) {
@@ -20,44 +32,37 @@ module.exports = function(feature, bbox) {
       });
     });
 
-    return buildFeature("MultiLineString", lines, feature.properties);
-  } else if (geom.type == "Polygon") {
-    var rings = clipPolygon(geom.coordinates, bbox);
-
-    return buildFeature("Polygon", rings, feature.properties);
-  } else if (geom.type == "MultiPolygon") {
-    var rings = geom.coordinates.map(function(polygon) {
+    return buildFeature('MultiLineString', lines, feature.properties);
+  } else if (geom.type === 'Polygon') {
+    return buildFeature('Polygon', clipPolygon(geom.coordinates, bbox), feature.properties);
+  } else if (geom.type === 'MultiPolygon') {
+    return buildFeature('MultiPolygon', geom.coordinates.map(function (polygon) {
       return clipPolygon(polygon, bbox);
-    }); 
-
-    return buildFeature("MultiPolygon", rings, feature.properties);
-  } else {
-
+    }), feature.properties);
   }
 };
 
-
 function clipPolygon(rings, bbox) {
-  var out_rings = [];
+  var outRings = [];
   rings.forEach(function(ring) {
     var clipped = lineclip.polygon(ring, bbox);
     if (clipped.length > 0) {
       if (clipped[0][0] !== clipped[clipped.length - 1][0] || clipped[0][1] !== clipped[clipped.length - 1][1]) {
         clipped.push(clipped[0]);
       }
-      out_rings.push(clipped);
+      outRings.push(clipped);
     }
   });
-  return out_rings;
+  return outRings;
 }
 
-function buildFeature(type, lines, properties) {
+function buildFeature (type, lines, properties) {
   return {
-    "type": "Feature",
-    "properties": properties || {},
-    "geometry": {
-      "type": type,
-      "coordinates": lines.slice()
+    'type': 'Feature',
+    'properties': properties || {},
+    'geometry': {
+      'type': type,
+      'coordinates': lines.slice()
     }
-  }
+  };
 }
